dd3745775c969c45c8ab97e93c1ba3fd53a0e9d6
Rohan17rp <rohan17.rp@gmail.com>
2019-07-25 13:50:26 +0530

    High score save feature

--- app/src/main/java/com/example/gamehub/game_interface.java
+++ app/src/main/java/com/example/gamehub/game_interface.java
@@ -1,5 +1,7 @@
 package com.example.gamehub;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,22 +15,26 @@ import android.widget.Toast;
 import java.util.Random;
 
 
+
+
 public class game_interface extends AppCompatActivity {
+    SharedPreferences highScore_save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stone_paper_scissors);
+        highScore_save = this.getSharedPreferences("highScoreKey" , Context.MODE_PRIVATE);
+        highScore = highScore_save.getInt("highScoreKey" , 0);
+        getHighScore();
     }
-    int streak = 0;
     String comp;
+    int streak = 0 , highScore = 0;
+    String streak_display = "0" , highScore_display = "0";
 
     public void comp(View cho) {
-
-
-
         TextView text = findViewById(R.id.textView_compChoice);
-        TextView count = findViewById(R.id.textView_streakCounter);
-
+        getStreak();
+        
         Random r = new Random();
         String t = String.format("%d", r.nextInt(3));
         switch (t) {
@@ -53,9 +59,11 @@ public class game_interface extends AppCompatActivity {
                         break;
                     case "1":
                         Toast.makeText(this,"Lost",Toast.LENGTH_LONG).show();
+                        streak = 0;
                         break;
                     case "2":
                         Toast.makeText(this,"Win",Toast.LENGTH_LONG).show();
+                        streak++;
                         break;
                 }
                 break;
@@ -91,8 +99,31 @@ public class game_interface extends AppCompatActivity {
                 break;
         }
 
+        setHighScore();
+        getStreak();
+        getHighScore();
+
     }
     public void exit(View exit){
+        setPreference();
         finish();
+    }
+    public void getStreak(){
+        streak_display = Integer.toString(streak);
+        TextView streakCount = findViewById(R.id.textView_streakCounter);
+        streakCount.setText(streak_display);
+    }
+    public void setHighScore(){
+        if(streak > highScore){
+            highScore = streak;
+        }
+    }
+    public void getHighScore(){
+        highScore_display = Integer.toString(highScore);
+        TextView streakCount = findViewById(R.id.textView_highScoreCounter);
+        streakCount.setText(highScore_display);
+    }
+    public  void setPreference(){
+                highScore_save.edit().putInt("highScoreKey" , highScore).commit();
     }
 }
